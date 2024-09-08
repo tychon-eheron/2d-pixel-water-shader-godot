98bf4e1c3f23349f4ccb3b9e58e2bbaa072e15d5
tychon <tychon@gmail.com>
2024-09-07 20:27:52 -0400

    Perf tweaks to Trails._Process

--- Scripts/Trails.cs
+++ Scripts/Trails.cs
@@ -15,39 +15,52 @@ public partial class Trails : Line2D {
 	float smallestTipWidth;
 	[Export]
 	float largestTipWidth;
-
-	float length;
-	Queue<Vector2> queue = new();
+	
+	float lastLength;
 	Vector2I offset;
+	List<Vector2> queue = new();
 	
 	StopwatchHistogram histogram;
 
 	public override void _Ready() {
 		offset = new Vector2I(subViewport.Size.X / 2, subViewport.Size.Y / 2);
 		histogram = new(GetNode<Label>("../../TimeLog"));
+		
+		var globalOffset = parent.GlobalPosition + offset;
+		var localOffset = parent.ToLocal(globalOffset);
+		for (int i = 0, ii = MAX_LENGTH; i < ii; ++i) {
+			queue.Add(globalOffset);
+			AddPoint(localOffset);
+		}
 	}
 
 	public override void _Process(double delta) {
 		using (histogram.Capture()) {
-			length = 0;
-
-			Vector2 pos = parent.GlobalPosition + offset;
-			queue.Enqueue(pos);
-			if (queue.Count > MAX_LENGTH && queue.Count > 2) {
-				queue.Dequeue();
+			Vector2 globalPosition = parent.GlobalPosition + offset;
+			float newSegmentLength = queue[queue.Count - 1].DistanceTo(globalPosition);
+			// Early exit if idle.
+			if (newSegmentLength < 0.0001f && lastLength < 0.0001f) {
+				return;
 			}
-
-			ClearPoints();
-
-			Vector2[] queueArray = queue.ToArray();
-			for (int i = 0; i < queue.Count - 1; i++) {
-				length += queueArray[i].DistanceTo(queueArray[i + 1]);
-				AddPoint(parent.ToLocal(queueArray[i]));
+			
+			// Remove length of first segment, add length of new last segment.
+			float length = lastLength - queue[0].DistanceTo(queue[1]) + newSegmentLength;
+			
+			// Shift points down one place.
+			for (int i = 1, ii = queue.Count; i < ii; ++i) {
+				queue[i - 1] = queue[i];
+				SetPointPosition(i - 1, parent.ToLocal(queue[i]));
 			}
-			AddPoint(parent.ToLocal(queueArray[queue.Count - 1]));
-
-			float widthValue = Mathf.Lerp(smallestTipWidth, largestTipWidth, Mathf.InverseLerp(0, distanceAtLargestWidth, length));
-			WidthCurve.SetPointValue(0, widthValue);
+			
+			// Set last point to new position.
+			queue[queue.Count - 1] = globalPosition;
+			SetPointPosition(queue.Count - 1, parent.ToLocal(globalPosition));
+			
+			if (lastLength != length) {
+				float widthValue = Mathf.Lerp(smallestTipWidth, largestTipWidth, Mathf.InverseLerp(0, distanceAtLargestWidth, length));
+				WidthCurve.SetPointValue(0, widthValue);
+				lastLength = length;
+			}
 		}
 	}
 }
